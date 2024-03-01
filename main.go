531618f6c1dcd786c1315b699492d2ff55f7be8c
@Arustat <mayssou839@gmail.com>
2024-03-01 12:22:55 +0100

    index

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import(
+	"fmt"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main(
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/test")
+    if err != nil {
+        panic(err.Error())
+    }
+    defer db.Close()
+    fmt.Println("Success!")
+)
