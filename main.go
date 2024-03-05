b82b9899026ee12bf99dc5a2619968abcb0bcdbc
@Arustat <mayssou839@gmail.com>
2024-03-05 11:22:57 +0100

    login page

--- main.go
+++ main.go
@@ -7,10 +7,5 @@ import(
 )
 
 func main(
-	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/test")
-    if err != nil {
-        panic(err.Error())
-    }
-    defer db.Close()
-    fmt.Println("Success!")
+	
 )
